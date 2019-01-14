3736c1c2765b698c5c4813c264639b8c31f07061
lyle8341 <lyllyl0000@126.com>
2019-01-14 22:21:09 +0800

    改成工程，方便测试

---
+++ src/main/java/com/lyle/db/DatabasesApplication.java
@@ -0,0 +1,14 @@
+package com.lyle.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DatabasesApplication.class, args);
+  }
+
+}
+
